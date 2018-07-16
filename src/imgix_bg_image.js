a752b968cd942ca1a7dda931b916a6fbe1330542
Sean C Davis <scdavis41@gmail.com>
2018-07-16 15:25:35 -0400

    Add background optimizer

---
+++ src/imgix_bg_image.js
@@ -0,0 +1,239 @@
+export default class ImgixBgImage {
+
+  constructor(el) {
+    // Length of time to complete fade-in transition.
+    this.timeToFade = 500;
+    // The primary element (i.e. the one with the background image).
+    this.el = $(el);
+    // Prepare the element and its container for optimization.
+    this.initEl();
+    // Kick off the optimization process.
+    this.initOptimization();
+  }
+
+  /**
+   * Load an image in memory (not within the DOM) with the same source as the
+   * placeholder image. Once that has completed, we know we're safe to begin
+   * processing.
+   */
+  initOptimization() {
+    $('<img>')
+      .on('load', $.proxy(this.renderTmpPlaceholderEl, this))
+      .attr('src', this.placeholderImgUrl);
+  }
+
+  // ---------------------------------------- | Main Element
+
+  /**
+   * Prepare the main element and its container for optimization.
+   */
+  initEl() {
+    this.setPlaceholderImgUrl();
+    this.setContainerTmpCss();
+    this.setElTmpCss();
+  }
+
+  /**
+   * Set reference to original image URL, which is expected to be a small
+   * placeholder.
+   */
+  setPlaceholderImgUrl() {
+    this.placeholderImgUrl = this.el.css('background-image')
+      .replace('url(', '')
+      .replace(')', '')
+      .replace(/\"/gi, "");
+  }
+
+  /**
+   * The parent of our jumbotron container should be relatively positioned
+   * (temporarily) so that we can absolutely position the temp image in the
+   * correct location.
+   */
+  setContainerTmpCss() {
+    this.el.parent().css('position', 'relative');
+  }
+
+  /**
+   * The main element must have a position set for it to be rendered on top of
+   * the temporary full-size image. We assume that if the element is not
+   * explicitly positioned absolutely, then it can safely be positioned
+   * relatively.
+   */
+  setElTmpCss() {
+    if (this.el.css('position') != 'absolute') {
+      this.el.css('position', 'relative');
+    }
+  }
+
+  // ---------------------------------------- | Placeholder Image (Temp)
+
+  /**
+   * Render a clone of the element with the background image directly behind
+   * itself.
+   */
+  renderTmpPlaceholderEl() {
+    this.initTmpPlaceholderEl();
+    this.setTmpPlaceholderElCss();
+    this.addTmpPlaceholderElToDom();
+    this.renderFullSizeImg();
+  }
+
+  /**
+   * Create a clone of the element with the background image. Remove content
+   * from the clone -- often elements with a background image contain content.
+   */
+  initTmpPlaceholderEl() {
+    this.tmpPlaceholderEl = this.el.clone();
+    this.tmpPlaceholderEl.html('');
+  }
+
+  /**
+   * Position the clone directly behind the main element
+   */
+  setTmpPlaceholderElCss() {
+    this.tmpPlaceholderEl.css({
+      position: 'absolute',
+      top: this.el.position().top,
+      left: this.el.position().left,
+      width: this.el.outerWidth(),
+      height: this.el.outerHeight(),
+      backgroundColor: 'transparent'
+    });
+  }
+
+  /**
+   * Add temporary element to the DOM, directly before the main element
+   * containing the background image.
+   */
+  addTmpPlaceholderElToDom() {
+    this.tmpPlaceholderEl.insertBefore(this.el);
+  }
+
+  // ---------------------------------------- | Full-Size Image (Temp)
+
+  /**
+   * Create another clone, this time of the temporary placeholder image. This
+   * new element sits behind the other two and is responsible for loading the
+   * full-size image.
+   */
+  renderFullSizeImg() {
+    this.removeElBgImg();
+    this.initTmpFullSizeEl();
+    this.setTmpFullSizeElImg();
+    this.addTmpFullSizeElToDom();
+    this.initTransition();
+  }
+
+  /**
+   * Remove the background color and image from the main element. The user won't
+   * notice this transition because the temp duplicate image is already set and
+   * is sitting behind the primary element.
+   *
+   * This also stores a reference to the original background color so we can put
+   * it back when the transition is complete.
+   */
+  removeElBgImg() {
+    this.elBgColor = this.el.css('background-color');
+    this.el.css('background-color', 'transparent');
+    this.el.css('background-image', '');
+  }
+
+  /**
+   * The temporary full-size element is a clone of the temporary placeholder
+   * image element.
+   */
+  initTmpFullSizeEl() {
+    this.tmpFullSizeEl = this.tmpPlaceholderEl.clone();
+  }
+
+  /**
+   * Sets a reference to the full-size image URL based on the current dimensions
+   * of the main element.
+   */
+  setFullSizeImgUrl() {
+    this.fullSizeImgUrl = this.placeholderImgUrl
+      .replace(/(\?|\&)(w=)(\d+)/i, '$1$2' + this.el.width())
+      .replace(/(\?|\&)(h=)(\d+)/i, '$1$2' + this.el.height());
+  }
+
+  /**
+   * Change the URL of this temporary element's background image to be the
+   * full-size image.
+   */
+  setTmpFullSizeElImg() {
+    this.setFullSizeImgUrl();
+    this.tmpFullSizeEl.css('background-image', `url("${this.fullSizeImgUrl}")`);
+  }
+
+  /**
+   * Add the temporary full-size element direct before the temporary placeholder
+   * element.
+   */
+  addTmpFullSizeElToDom() {
+    this.tmpFullSizeEl.insertBefore(this.tmpPlaceholderEl);
+  }
+
+  // ---------------------------------------- | Transition
+
+  /**
+   * Load full-size image in memory. When it has loaded we can confidentally
+   * fade out the placeholder, knowing the full-size image will be in its place.
+   */
+  initTransition() {
+    $('<img>')
+      .on('load', $.proxy(this.transitionImg, this))
+      .attr('src', this.fullSizeImgUrl);
+  }
+
+  /**
+   * Fade out the temporary placeholder, set the background-image on the main
+   * element to the full-size URL, then remove the temporary elements behind the
+   * main element
+   */
+  transitionImg() {
+    this.fadeOuttmpPlaceholderEl();
+    setTimeout($.proxy(function() {
+      this.updateElImg();
+      this.replaceElTmpCss();
+      this.removeTmpEls();
+    }, this), this.timeToFade);
+  }
+
+  /**
+   * Fade out the placeholder element. This was the temporary clone of the main
+   * element that has a placeholder background image.
+   *
+   * Rememeber the main element's background image was unset and its color set
+   * to transparent. That is why fading out this temporary image will work
+   * properly.
+   */
+  fadeOuttmpPlaceholderEl() {
+    this.tmpPlaceholderEl.fadeTo(this.timeToFade, 0);
+  }
+
+  /**
+   * Reset the image URL (this helps if the size of the element has changed),
+   * then set the background image to the new source.
+   */
+  updateElImg() {
+    this.setFullSizeImgUrl();
+    this.el.css('background-image', `url('${this.fullSizeImgUrl}')`);
+  }
+
+  /**
+   * Set the background color back to what it was before the transition.
+   */
+  replaceElTmpCss() {
+    this.el.css('background-color', this.elBgColor);
+  }
+
+  /**
+   * Remove both temporary elements from the DOM.
+   */
+  removeTmpEls() {
+    this.tmpPlaceholderEl.remove();
+    this.tmpFullSizeEl.remove();
+    this.tmpPlaceholderEl = undefined;
+    this.tmpFullSizeEl = undefined;
+  }
+}
--- src/optimizer.js
+++ src/optimizer.js
@@ -1,26 +1,39 @@
 import ImgixImage from './imgix_image';
+import ImgixBgImage from './imgix_bg_image';
 
 export default class Optimizer {
 
   constructor(options = {}) {
     this.initOptions(options);
     this.optimizeImages();
+    this.optimizeBgImages();
   }
+
+  // ---------------------------------------- | Options
 
   initOptions(options = {}) {
     this.options = options;
     const defaultOptions = {
-      parent: 'body',
-      selector: 'img[data-optimize-img]'
+      parent: 'body'
     }
     for (const [key, value] of Object.entries(defaultOptions)) {
       if (!this.options[key]) { this.options[key] = value; }
     }
   }
 
+  // ---------------------------------------- | Inline Images
+
   optimizeImages() {
-    for (const img of $(`${this.options.parent} ${this.options.selector}`)) {
-      const image = new ImgixImage(img);
+    for (const img of $(`${this.options.parent} img[data-optimize-img]`)) {
+      new ImgixImage(img);
+    }
+  }
+
+  // ---------------------------------------- | Background Images
+
+  optimizeBgImages() {
+    for (const img of $(`${this.options.parent} [data-optimize-bg-img]`)) {
+      new ImgixBgImage(img);
     }
   }
 
