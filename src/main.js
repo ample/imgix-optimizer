49381917b3375da7bb042c8391925ee18b400956
Sean C Davis <scdavis41@gmail.com>
2018-07-20 16:25:00 -0400

    Surface ImgixBgImage and ImgixImage on the Imgix object

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+import ImgixBgImage from './imgix_bg_image';
+import ImgixImage from './imgix_image';
 import Optimizer from './optimizer';
 
 window['Imgix'] = window['Imgix'] || {};
 
+Imgix.ImgixBgImage = ImgixBgImage;
+Imgix.ImgixImage = ImgixImage;
 Imgix.Optimizer = Optimizer;
